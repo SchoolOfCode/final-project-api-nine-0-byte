ab3abeaf8e818b454d1af04d309c0adea5e8f5c2
Samira Sabirin <93654977+samira-s-midgley@users.noreply.github.com>
2022-03-02 16:17:40 +0000

    populate the table

--- db/scripts/filters/populateTable.js
+++ db/scripts/filters/populateTable.js
@@ -1,10 +1,25 @@
 import db from "../../connection.js";
+import { filters } from "../../../utils/dummyFilter.js";
 
-const response = await db.query(
-  `INSERT INTO users (username, first_name, last_name) VALUES ($1, $2, $3);`,
-  ["iseecode", "Chris", "Code"]
-);
+async function populateTable() {
+  for (let i = 0; i < filters.length; i++) {
+    const price = filters[i][2];
+    const connectorType = filters[i][3];
+    const availability = filters[i][4];
+    const res = await db.query(
+      `INSERT INTO filters (price, connector_type, availability) VALUES ($1, $2, $3) RETURNING *;`,
+      [price, connectorType, availability]
+    );
 
-console.log(response);
+    console.log("populated table", res);
+  }
+}
+
+// // const response = await db.query(
+// //   `INSERT INTO users (username, first_name, last_name) VALUES ($1, $2, $3);`,
+// //   ["iseecode", "Chris", "Code"]
+// );
+
+// console.log(response);
 
 db.end();
---
+++ utils/dummyFilter.js
@@ -0,0 +1,19 @@
+export const filters = [
+  [
+    1,
+    1,
+    0.1,
+    [
+      "JEVS G105 (CHAdeMO) DC",
+      "Type 1 SAEJ1772 (IEC 62196)",
+      "Type 3 Scame (IEC62196)",
+      "CCS Type 2 Combo (IEC62196)",
+      "Type 2 Tesla (IEC62196) DC",
+      "Commando 2P+E (IEC60309)",
+      "Commando 3P+N+E (IEC60309)",
+    ],
+    true,
+  ],
+  [2, 2, 0, ["Type 3 Scame (IEC62196)", "CCS Type 2 Combo (IEC62196)"], true],
+  [3, 3, 0.5, ["Type 2 Mennekes (IEC62196)"], false],
+];
