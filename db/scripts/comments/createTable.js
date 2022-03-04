b2d920b6185a136c1f892f6acd180e34cb29bded
AleksApilats <shurikapilats@googlemail.com>
2022-03-04 12:19:59 +0000

    created dummy data forcomments table

--- db/scripts/comments/createTable.js
+++ db/scripts/comments/createTable.js
@@ -1,7 +1,7 @@
 import db from "../../connection.js";
 
 const response = await db.query(
-  `CREATE TABLE IF NOT EXISTS comments (comment_id SERIAL PRIMARY KEY, user_id INTEGER, location TEXT, comment TEXT, date TIMESTAMP, visibility BOOLEAN );`
+  `CREATE TABLE IF NOT EXISTS comments (comment_id SERIAL PRIMARY KEY, user_id TEXT, location TEXT, comment TEXT, date TIMESTAMP, visibility BOOLEAN );`
 );
 
 console.log(response);
